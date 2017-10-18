aff787a1e5b3fd1125d628d748d30ffc2dded33b
Peter Thompson <petethegreat@gmail.com>
2017-10-18 11:30:23 -0400

    need to fix stuff

--- knitStuff.R
+++ knitStuff.R
@@ -3,6 +3,7 @@
 
 library(rmarkdown)
 render('Stuff.Rmd',output_format='html_document')
+render('Pyspark_ML.Rmd',output_format='html_document')
 # change output to all once things are looking good
 
 
